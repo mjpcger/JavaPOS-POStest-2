a8282b0e4a891ad5ae95cc00f839bc753f1e2a65
Martin <37701263+mjpcger@users.noreply.github.com>
2020-02-23 01:39:43 +0100

    MSR rework
    - GUI updated.
    - CheckBox for track conversion added. Depending on DecodeData setting, ASCII or Hexadecimal are the prefered settings.
    - ListView to show tracks read added.
    - DataEventEnabled checkbox added.
    - Initialization and update of GUI objects rewritten.

--- src/main/java/postest2/MSRController.java
+++ src/main/java/postest2/MSRController.java
@@ -26,6 +26,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import jpos.JposException;
 import jpos.MSR;
 
+import jpos.MSRConst;
+import jpos.events.DataEvent;
 import org.apache.xerces.parsers.DOMParser;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
@@ -63,6 +65,8 @@ public class MSRController extends CommonController implements Initializable {
 	public TextField writeTracks_timeout;
 
 	@FXML
+	public ComboBox<String> showData;
+	@FXML
 	public ComboBox<String> dataEncryptionAlgorithm;
 	@FXML
 	public ComboBox<Boolean> decodeData;
@@ -78,15 +82,44 @@ public class MSRController extends CommonController implements Initializable {
 	public ComboBox<Boolean> transmitSentinels;
 	@FXML
 	public ListView<String> writeTracks_data;
+	@FXML
+	public ListView<String> trackData;
 
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		setUpTooltips();
+		setUpShowData();
 		service = new MSR();
 		((MSR) service).addStatusUpdateListener(this);
+		((MSR) service).addDataListener(this);
 		RequiredStateChecker.invokeThis(this, service);
 		setUpLogicalNameComboBox("MSR");
+	}
+
+	private void setUpShowData() {
+		String current = showData.getValue();
+		showData.getItems().clear();
+		showData.getItems().add("Hexadecimal");
+		showData.getItems().add("ASCII");
+		showData.getItems().add("Decoded");
+		showData.getItems().add("Length");
+		showData.getSelectionModel().select(current == null ? "ASCII" : current);
+	}
+
+	@FXML
+	public void handleShowData(ActionEvent actionEvent) {
+		String conversion = showData.getValue();
+		if ("Hexadecimal".equals(conversion)) {
+			DeviceProperties.setConversion(service, DeviceProperties.ByteConversion.Hexadecimal);
+		} else if ("ASCII".equals(conversion)) {
+			DeviceProperties.setConversion(service, DeviceProperties.ByteConversion.Ascii);
+		} else if ("Length".equals(conversion)) {
+			DeviceProperties.setConversion(service, DeviceProperties.ByteConversion.Length);
+		} else if ("Decoded".equals(conversion)) {
+			DeviceProperties.setConversion(service, DeviceProperties.ByteConversion.Decoded);
+		}
+		setUpTrackData();
 	}
 
 	/* ************************************************************************
@@ -99,16 +132,15 @@ public class MSRController extends CommonController implements Initializable {
 		try {
 			if (deviceEnabled.isSelected()) {
 				((MSR) service).setDeviceEnabled(true);
-				setUpComboBoxes();
-				setUpListViews();
 			} else {
 				((MSR) service).setDeviceEnabled(false);
 			}
-			RequiredStateChecker.invokeThis(this, service);
 		} catch (JposException je) {
 			JOptionPane.showMessageDialog(null, je.getMessage());
 			je.printStackTrace();
 		}
+		setupGuiObjects();
+		RequiredStateChecker.invokeThis(this, service);
 	}
 
 	/**
@@ -193,6 +225,7 @@ public class MSRController extends CommonController implements Initializable {
 			JOptionPane.showMessageDialog(null, e1.getMessage());
 			e1.printStackTrace();
 		}
+		setUpTrackData();
 	}
 
 	@FXML
@@ -213,6 +246,7 @@ public class MSRController extends CommonController implements Initializable {
 			JOptionPane.showMessageDialog(null, e1.getMessage());
 			e1.printStackTrace();
 		}
+		setUpTrackData();
 	}
 
 	@FXML
@@ -223,6 +257,7 @@ public class MSRController extends CommonController implements Initializable {
 			JOptionPane.showMessageDialog(null, e1.getMessage());
 			e1.printStackTrace();
 		}
+		setUpTrackData();
 	}
 
 	@FXML
@@ -243,6 +278,7 @@ public class MSRController extends CommonController implements Initializable {
 			JOptionPane.showMessageDialog(null, e1.getMessage());
 			e1.printStackTrace();
 		}
+		setUpTrackData();
 	}
 
 	@FXML
@@ -398,6 +434,18 @@ public class MSRController extends CommonController implements Initializable {
 			writeTracks_data.getItems().add(writeTracks_addData.getText());
 		}
 	}
+
+	@FXML
+	public void handleDataEventEnable(ActionEvent actionEvent) {
+		try {
+			((MSR) service).setDataEventEnabled(dataEventEnabled.isSelected());
+		} catch (JposException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+			e.printStackTrace();
+			dataEventEnabled.setSelected(!dataEventEnabled.isSelected());
+		}
+	}
+
 	/*
 	 * Set Up ComboBoxes
 	 */
@@ -406,51 +454,96 @@ public class MSRController extends CommonController implements Initializable {
 		dataEncryptionAlgorithm.getItems().clear();
 		dataEncryptionAlgorithm.getItems().add(MSRConstantMapper.MSR_DE_3DEA_DUKPT.getConstant());
 		dataEncryptionAlgorithm.getItems().add(MSRConstantMapper.MSR_DE_NONE.getConstant());
-		dataEncryptionAlgorithm.setValue(MSRConstantMapper.MSR_DE_NONE.getConstant());
+		String current = DeviceProperties.getPropertyValue(service, new MSRConstantMapper(), "getDataEncryptionAlgorithm");
+		dataEncryptionAlgorithm.getSelectionModel().select(current);
 		
 	}
 	
 	private void setUpDecodeData(){
+		boolean current = false;
+		try {
+			current = ((MSR)service).getDecodeData();
+		} catch (JposException e) {
+			if (getDeviceState(service) != JposState.CLOSED) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+				e.printStackTrace();
+			}
+		}
 		decodeData.getItems().clear();
 		decodeData.getItems().add(true);
 		decodeData.getItems().add(false);
-		decodeData.setValue(true);
-		
+		decodeData.getSelectionModel().select(current);
 	}
 	
 	private void setUpErrorReportingType(){
 		errorReportingType.getItems().clear();
 		errorReportingType.getItems().add(MSRConstantMapper.MSR_ERT_CARD.getConstant());
 		errorReportingType.getItems().add(MSRConstantMapper.MSR_ERT_TRACK.getConstant());
-		errorReportingType.setValue(MSRConstantMapper.MSR_ERT_CARD.getConstant());
+		String current = DeviceProperties.getPropertyValue(service, new MSRConstantMapper(), "getErrorReportingType");
+		errorReportingType.getSelectionModel().select(current);
 	}
 	
 	private void setUpParseDecodeData(){
+		boolean current = false;
+		try {
+			current = ((MSR)service).getParseDecodeData();
+		} catch (JposException e) {
+			if (getDeviceState(service) != JposState.CLOSED) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+				e.printStackTrace();
+			}
+		}
 		parseDecodeData.getItems().clear();
 		parseDecodeData.getItems().add(true);
 		parseDecodeData.getItems().add(false);
-		parseDecodeData.setValue(true);
+		parseDecodeData.getSelectionModel().select(current);
 	}
 	
 	private void setUpTracksToWrite(){
+		String current = DeviceProperties.getPropertyValue(service, new MSRConstantMapper(), "getTracksToWrite");
+		int capwrite = 0;
+		try {
+			capwrite = ((MSR)service).getCapWritableTracks();
+		} catch (JposException e) {
+			if (getDeviceState(service) == JposState.ENABLED) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+				e.printStackTrace();
+			}
+		}
 		tracksToWrite.getItems().clear();
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_2.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_2_3.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_2_3_4.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_2_4.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_3.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_3_4.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_4.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_2.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_2_3.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_2_3_4.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_2_4.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_3.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_3_4.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_4.getConstant());
-		tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_NONE.getConstant());
-		tracksToWrite.setValue(MSRConstantMapper.MSR_TR_1.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_NONE) == MSRConst.MSR_TR_NONE)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_NONE.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_1) == MSRConst.MSR_TR_1)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_1_2) == MSRConst.MSR_TR_1_2)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_2.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_1_2_3) == MSRConst.MSR_TR_1_2_3)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_2_3.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_1_2_3_4) == MSRConst.MSR_TR_1_2_3_4)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_2_3_4.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_1_2_4) == MSRConst.MSR_TR_1_2_4)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_2_4.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_1_3) == MSRConst.MSR_TR_1_3)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_3.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_1_3_4) == MSRConst.MSR_TR_1_3_4)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_3_4.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_1_4) == MSRConst.MSR_TR_1_4)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_1_4.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_2) == MSRConst.MSR_TR_2)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_2.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_2_3) == MSRConst.MSR_TR_2_3)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_2_3.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_2_3_4) == MSRConst.MSR_TR_2_3_4)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_2_3_4.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_2_4) == MSRConst.MSR_TR_2_4)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_2_4.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_3) == MSRConst.MSR_TR_3)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_3.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_3_4) == MSRConst.MSR_TR_3_4)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_3_4.getConstant());
+		if ((capwrite & MSRConst.MSR_TR_4) == MSRConst.MSR_TR_4)
+			tracksToWrite.getItems().add(MSRConstantMapper.MSR_TR_4.getConstant());
+		tracksToWrite.getSelectionModel().select(current);
 	}
 	
 	private void setUpTracksToRead(){
@@ -471,17 +564,29 @@ public class MSRController extends CommonController implements Initializable {
 		tracksToRead.getItems().add(MSRConstantMapper.MSR_TR_3_4.getConstant());
 		tracksToRead.getItems().add(MSRConstantMapper.MSR_TR_4.getConstant());
 		tracksToRead.getItems().add(MSRConstantMapper.MSR_TR_NONE.getConstant());
-		tracksToRead.setValue(MSRConstantMapper.MSR_TR_1.getConstant());
+		String current = DeviceProperties.getPropertyValue(service, new MSRConstantMapper(), "getTracksToRead");
+		tracksToRead.getSelectionModel().select(current);
 	}
 	
 	private void setUpTransmitSentinels(){
+		boolean current = false;
+		try {
+			current = ((MSR)service).getTransmitSentinels();
+		} catch (JposException e) {
+			if (getDeviceState(service) != JposState.CLOSED) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+				e.printStackTrace();
+			}
+		}
 		transmitSentinels.getItems().clear();
 		transmitSentinels.getItems().add(true);
 		transmitSentinels.getItems().add(false);
-		transmitSentinels.setValue(true);
+		transmitSentinels.setValue(current);
 	}	
-	
-	private void setUpComboBoxes(){
+
+	@Override
+	public void setupGuiObjects(){
+		super.setupGuiObjects();
 		setUpDataEncryptionAlgorithm();
 		setUpDecodeData();
 		setUpErrorReportingType();
@@ -489,9 +594,65 @@ public class MSRController extends CommonController implements Initializable {
 		setUpTracksToRead();
 		setUpTracksToWrite();
 		setUpTransmitSentinels();
+		setUpListViews();
+		setUpTrackData();
 	}
-	
+
+	private void setUpTrackData() {
+		MSRConstantMapper mapper = new MSRConstantMapper();
+		trackData.getItems().clear();
+		trackData.setStyle("-fx-font-family: 'monospaced';");
+		boolean encrypted = false;
+		int availabletracks = 0;
+		try {
+			encrypted = (((MSR)service).getCapDataEncryption() != MSRConst.MSR_DE_NONE ?
+					((MSR)service).getDataEncryptionAlgorithm() != MSRConst.MSR_DE_NONE : false) &&
+					((MSR)service).getCapTrackDataMasking() == false;
+			availabletracks = ((MSR)service).getTracksToRead();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if ((availabletracks & MSRConst.MSR_TR_1) == 0)
+			trackData.getItems().add("");
+		else if (encrypted)
+			trackData.getItems().add("[" +
+					DeviceProperties.getPropertyValue(service, mapper, "getTrack1EncryptedDataLength") +
+					" encrypted bytes]");
+		else
+			trackData.getItems().add(DeviceProperties.getPropertyValue(service, mapper, "getTrack1Data"));
+		if ((availabletracks & MSRConst.MSR_TR_2) == 0)
+			trackData.getItems().add("");
+		else if (encrypted)
+			trackData.getItems().add("[" +
+					DeviceProperties.getPropertyValue(service, mapper, "getTrack2EncryptedDataLength") +
+					" encrypted bytes]");
+		else
+			trackData.getItems().add(DeviceProperties.getPropertyValue(service, mapper, "getTrack2Data"));
+		if ((availabletracks & MSRConst.MSR_TR_3) == 0)
+			trackData.getItems().add("");
+		else if (encrypted)
+			trackData.getItems().add("[" +
+					DeviceProperties.getPropertyValue(service, mapper, "getTrack3EncryptedDataLength") +
+					" encrypted bytes]");
+		else
+			trackData.getItems().add(DeviceProperties.getPropertyValue(service, mapper, "getTrack3Data"));
+		if ((availabletracks & MSRConst.MSR_TR_4) == 0)
+			trackData.getItems().add("");
+		else if (encrypted)
+			trackData.getItems().add("[" +
+					DeviceProperties.getPropertyValue(service, mapper, "getTrack4EncryptedDataLength") +
+					" encrypted bytes]");
+		else
+			trackData.getItems().add(DeviceProperties.getPropertyValue(service, mapper, "getTrack4Data"));
+	}
+
 	private void setUpListViews(){
 		writeTracks_data.getItems().clear();
+	}
+
+	@Override
+	public void dataOccurred(DataEvent ev) {
+		super.dataOccurred(ev);
+		setUpTrackData();
 	}
 }
